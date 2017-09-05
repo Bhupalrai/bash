73cd7d95e5499e06bb79816a1599d778af7758a3
Bhupalrai <nuraeen.rai@gmail.com>
2017-09-05 17:56:28 +0545

    Create backup_users.sh

---
+++ backup_users.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#######################################################################
+#                    Backup linux users
+#----------------------------------------------------------------------
+# Backup all users including root
+#
+#######################################################################
+
+#
+# globals
+users_list=()
+
+#
+# add users to list
+users_list+=(root)
+for username in `ls /home`; do
+        users_list+=(${username})
+done
+
+#
+# inform user before sync
+echo "Syncing users"
+echo "-----------------------------------------"
+for username in ${users_list[@]}; do
+        echo $username
+done
+
+#
+# sync
+for username in ${users_list[@]}; do
+        dest_dir="/backup/rpy_1_backup"
+        [ $? -ne 0 ] && { echo "error creating directory ${dest_dir}, skipping..."; continue; }
+
+        if [ "${username}" = "root" ]; then
+                source_home="/root"
+        else
+                source_home="/home/${username}"
+        fi
+        rsync -rlptDq  --no-links "${source_home}" "${dest_dir}"
+done
+
+#
+# done
+echo "script complete"
