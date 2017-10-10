15a3540440bfa36689cb556d09f31be668b46c12
Bhupalrai <nuraeen.rai@gmail.com>
2017-10-10 09:19:01 +0545

    Update device_temp.sh

--- device_temp.sh
+++ device_temp.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #
 #@Author Bhupal Rai
+#######################################################################
+#                    Monitor raspberrypi board temperature
+#----------------------------------------------------------------------
+# Shutdown device whenever device overheats
+#
+#######################################################################
 
 max_temp_tolrt=76
 showin_std=false  #take action
@@ -9,10 +15,12 @@ if [ "${1}" = "d" ]; then
     showin_std=true
 fi
 
+#
 # cpu temp celcius
 cpu_temp_cmd=`cat /sys/class/thermal/thermal_zone0/temp`
 cpu_temp="CPU TEMP : $((cpu_temp_cmd/1000)) 'c"
 
+#
 # device temp, celcius
 dvtmpval=`vcgencmd measure_temp | cut -f 2 -d "="|cut -f 1 -d "'"`
 device_temp="RPY TEMP : ${dvtmpval} 'C"
@@ -28,9 +36,9 @@ tmp=`echo ${dvtmpval} | cut -f1 -d "."`
 r_up_dvtmpval=`expr $(echo ${tmp}) + 1`
 
 if [ $( echo $r_up_dvtmpval ) -gt $( echo ${max_temp_tolrt}) ]; then
+    # prevent device burning, poweroff
     /usr/bin/logger "Shutting down due to SoC temp ${r_up_dvtmpval}."
     /sbin/shutdown -h now
-
 else
    /usr/bin/logger "Current temperature. RPY: ${r_up_dvtmpval}'C, CPU: $((cpu_temp_cmd/1000))'C"
 fi
